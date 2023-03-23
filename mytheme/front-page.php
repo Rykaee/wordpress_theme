ae82b91d00aa12ee154f8f78cad8773fd98a2dc4
Roosa Kontinen <roosa.kontinen@hotmail.com>
2023-03-23 23:43:07 +0200

    Added simple nav bar and its functionality

--- mytheme/front-page.php
+++ mytheme/front-page.php
@@ -1,13 +1,15 @@
-<?php get_header('secondary'); ?>
+<?php get_header(); ?>
 
 
-<div class="container">
+<section class="page-wrap">
 
-    <h1><?php the_title();?></h1>
+    <div class="container">
+        <h1><?php the_title();?></h1>
+        <?php get_template_part('includes/section', 'content');?>
+    </div>
 
-    <?php get_template_part('includes/section', 'content');?>
+</section>
 
-</div>
 
 
 
--- mytheme/functions.php
+++ mytheme/functions.php
@@ -1,16 +1,27 @@
 <?php
 
 
+/*
+Functions for loading Bootstrap and your own css and using them.
+*/
+
+// Loading Bootstrap css and your own css-file.
+// REMEMBER put your own stylesheet under Bootstrap, so it will override Bootstrap's stylesheet.
 function load_css(){
 
+    //Bootstrap stylesheet
     wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
     wp_enqueue_style('bootstrap');
+
+    //Your stylesheet
+    wp_register_style('main', get_template_directory_uri() . '/css/main.css', array(), false, 'all');
+    wp_enqueue_style('main');
 
 }
 
 add_action('wp_enqueue_scripts', 'load_css');
 
-
+//Loading Bootstrap js and also jquery.
 function load_js(){
 
     wp_enqueue_script('jquery');
@@ -20,3 +31,17 @@ function load_js(){
 }
 
 add_action('wp_enqueue_scripts', 'load_js');
+
+
+//Theme option for adding wp menus.
+add_theme_support('menus');
+
+//Theme menus
+
+register_nav_menus(
+    array(
+        'top-menu' => 'Top Menu Location',
+        'mobile-menu' => 'Mobile Menu Location',
+        'footer-menu' => 'Footer Menu Location',
+    )
+);
